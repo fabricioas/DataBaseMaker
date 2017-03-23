ddd69c1f24f2f990b78d833a7f0bafba452e2978
fabricioalves <fabricioalves@cwinet.com.br>
2017-03-23 15:23:48 -0300

    #1 Criar Projeto WEB

---
+++ src/main/java/br/com/fabrleadr/DataBaseMakerConfiguration.java
@@ -0,0 +1,12 @@
+package br.com.fabrleadr;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("br.com.fabrleadr")
+public class DataBaseMakerConfiguration {
+
+}
---
+++ src/main/java/br/com/fabrleadr/StartDataBaseMaker.java
@@ -0,0 +1,9 @@
+package br.com.fabrleadr;
+
+import org.springframework.boot.SpringApplication;
+
+public class StartDataBaseMaker {
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseMakerConfiguration.class, args);
+	}
+}
